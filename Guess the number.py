58f04be9d4f46a1ff35c5ddc3736429f37581309
koteotiashvili <otiashvilikote@gmail.com>
2024-03-05 17:56:29 +0400

    second commit I added while to create loop, if elif  statements to define how game would work, and try except to highlight errors.

--- Guess the number.py
+++ Guess the number.py
@@ -10,3 +10,25 @@ def guess_number():
     attempt = 6
     # use while to creat loop
     guessed = False
+
+    while not guessed and attempt > 0:
+        try:
+            # after that we just use if elif else to give statement to our code
+            guess = int(input("Guess a number between 1 and 10"))
+            if guess == number:
+                print(f"Congratulations! You won, You guessed {number}!!")
+            # use elif statement to give our code function if user guessed number wrong they lose attempt
+            elif guess > number:
+                print(f'number is lower than {guess}, u have: {attempt - 1} tries left')
+                attempt -= 1
+            elif guess < number:
+                print(f'number is higher than {guess}, u have: {attempt - 1} tries left')
+            # and use except  to Value our error if user will not put number
+        except ValueError:
+            print(f"You must use a number, u have {attempt - 1} tries left")
+            attempt -= 1
+        if not guessed:
+            print(f'you ran out of attempts, u lost,number was {number}')
+
+
+guess_number()
