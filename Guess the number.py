c030589fa638b77f7b86121e1a639819d7a372bb
koteotiashvili <otiashvilikote@gmail.com>
2024-03-05 18:16:11 +0400

    third commit code had some problems and missed some parts, so I modified it

--- Guess the number.py
+++ Guess the number.py
@@ -14,15 +14,17 @@ def guess_number():
     while not guessed and attempt > 0:
         try:
             # after that we just use if elif else to give statement to our code
-            guess = int(input("Guess a number between 1 and 10"))
+            guess = int(input("Guess a number between 1 and 10: "))
             if guess == number:
                 print(f"Congratulations! You won, You guessed {number}!!")
+                guessed = True
             # use elif statement to give our code function if user guessed number wrong they lose attempt
             elif guess > number:
                 print(f'number is lower than {guess}, u have: {attempt - 1} tries left')
                 attempt -= 1
             elif guess < number:
                 print(f'number is higher than {guess}, u have: {attempt - 1} tries left')
+                attempt -= 1
             # and use except  to Value our error if user will not put number
         except ValueError:
             print(f"You must use a number, u have {attempt - 1} tries left")
